596fb00a5c7742ada9322d2d16dac265c3cae1f4
limaojunio <ecjrcl@gmail.com>
2025-09-22 11:51:41 -0300

    Config Banco de Dados, Módulo Postagem Controller e Service "Método: Listar tudo"

---
+++ src/postagem/postagem.module.ts
@@ -0,0 +1,15 @@
+import { Module, Post } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Postagem } from './entities/postagem.entity';
+import { PostagemController } from './controllers/postagem.controller';
+import { PostagemService } from './services/postagem.service';
+
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Postagem])],
+    providers: [PostagemService],
+    controllers: [PostagemController],
+    exports: [],
+})
+
+export class PostagemModule {}
