9f8e23027fa33b4768cab145f1756b41d09bae76
limaojunio <ecjrcl@gmail.com>
2025-09-25 11:42:12 -0300

    Implementação do Módulo Tema.

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,9 +1,11 @@
 import { IsNotEmpty } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Tema } from "../../tema/entities/tema.entity";
 
 @Entity({name: "tb_postagens"})
 
 export class Postagem {
+    
     @PrimaryGeneratedColumn()
     id: number;
 
@@ -17,4 +19,9 @@ export class Postagem {
 
     @UpdateDateColumn()
     data: Date;
+
+    @ManyToOne(() => Tema, (tema) => tema.postagem, {
+        onDelete: "CASCADE"
+    })
+    tema: Tema;
 }
---
+++ src/tema/tema.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Type } from 'class-transformer';
+import { TemaController } from './controllers/tema.controller';
+import { Tema } from './entities/tema.entity';
+import { TemaService } from './services/tema.service';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Tema])],
+    controllers: [TemaController],
+    providers: [TemaService],
+    exports: [TemaService]
+})
+export class TemaModule {}
