ba6690fc6a23af68dcf4bc41892332df48ebb7bb
limaojunio <ecjrcl@gmail.com>
2025-10-20 13:10:25 -0300

    Deploy do projeto Blog Pessoa

--- src/tema/controllers/tema.controller.ts
+++ src/tema/controllers/tema.controller.ts
@@ -11,12 +11,15 @@ import {
   Put,
   UseGuards,
 } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 import { Tema } from '../entities/tema.entity';
 import { TemaService } from '../services/tema.service';
 
+@ApiTags('Tema')
 @UseGuards(JwtAuthGuard)
 @Controller('/temas')
+@ApiBearerAuth()
 export class TemaController {
   constructor(private readonly temaService: TemaService) {}
 
@@ -34,7 +37,7 @@ export class TemaController {
 
   @Get('/descricao/:descricao')
   @HttpCode(HttpStatus.OK)
-  findAllBydescricao(@Param('descricao') descricao: string): Promise<Tema[]> {
+  findBydescricao(@Param('descricao') descricao: string): Promise<Tema[]> {
     return this.temaService.findAllByDescricao(descricao);
   }
 
--- src/tema/entities/tema.entity.ts
+++ src/tema/entities/tema.entity.ts
@@ -1,16 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { Postagem } from '../../postagem/entities/postagem.entity';
 
 @Entity({ name: 'tb_temas' })
 export class Tema {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @IsNotEmpty()
   @Column({ length: 255, nullable: false })
+  @ApiProperty()
   descricao: string;
 
+  @ApiProperty()
   @OneToMany(() => Postagem, (postagem) => postagem.tema)
   postagem: Postagem[];
 }
